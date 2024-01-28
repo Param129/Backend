49029f74d9571328886aa18deac43cf04a0443d8
Param129 <paramtomar81@gmail.com>
2024-01-28 11:26:15 +0530

    routers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
@@ -9,8 +10,8 @@ dotenv.config({
 
 
 connectDB().then(()=>{
-  app.listen(process.env.PORT || 3000,()=>{
-    console.log(`server is running at port : ${process.env.PORT}`)
+  app.listen(process.env.PORT || 4000,()=>{
+    console.log(`⚙️  server is running at port : ${process.env.PORT}`)
   })
 })
 .catch((error)=>{
---
+++ src/routes/user.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+const router=Router();
+
+
+import {registerUser} from "../controllers/user.controller.js"
+
+
+router.route("/register").post(registerUser)
+
+
+export default router;
